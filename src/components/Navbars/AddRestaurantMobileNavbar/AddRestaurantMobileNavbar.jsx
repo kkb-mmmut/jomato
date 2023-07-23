d5cfc2b9808065b51c4394277869a6c7d8b52efc
Kamlesh Kumar Bind <kkb.mmmut@gmail.com>
2023-07-23 16:50:29 +0530

    initial:deploy

--- src/components/Navbars/AddRestaurantMobileNavbar/AddRestaurantMobileNavbar.jsx
+++ src/components/Navbars/AddRestaurantMobileNavbar/AddRestaurantMobileNavbar.jsx
@@ -18,7 +18,7 @@ let AddRestaurantMobileNavbar = ({ toogleMenu, setToggleMenu }) => {
     return <div className={css.navbarH}>
         <div className={css.menu}>
             <img className={css.menuBar} src={close} alt='menu bar' onClick={() => setToggleMenu(val => !val)} />
-            <Link to='/' className={css.title}>Jomato</Link>
+            <Link to='/jomato' className={css.title}>Jomato</Link>
         </div>
         <div className={css.navbar} onClick={() => setState(val => !val)}>
             <span className={css.profile}>
--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route index element={<App />} />
-      <Route path="/" element={<App />} />
+      <Route path="/jomato" element={<App />} />
       <Route path="/add-restaurant" element={<AddRestaurant />} />
       <Route path="/show-case" element={<ShowCase />} /> 
       <Route path="/user/:userId/notifications" element={<SkipedPage />} />
